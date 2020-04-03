59f4eee1c956bfb3787b1c1640d0beea392b95fd
gerardo rodriguez <theronin115@gmail.com>
2020-04-03 07:08:47 -0600

    Updates

---
+++ precache-manifest.96ba5dcbe98fb3d31312872953d679f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48cfb2d9c1bf3be226325437badaa3d",
+    "url": "/RoundRobin/index.html"
+  },
+  {
+    "revision": "86e2b813b2f266c777dd",
+    "url": "/RoundRobin/static/css/main.cf6c2552.chunk.css"
+  },
+  {
+    "revision": "4afaacb35c8000419b06",
+    "url": "/RoundRobin/static/js/2.f2922cb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoundRobin/static/js/2.f2922cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e2b813b2f266c777dd",
+    "url": "/RoundRobin/static/js/main.5877ffc9.chunk.js"
+  },
+  {
+    "revision": "630c5532eb1347de822d",
+    "url": "/RoundRobin/static/js/runtime-main.918da480.js"
+  }
+]);
